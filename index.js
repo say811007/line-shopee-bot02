2a41b04168b5cb0da26a659fb23e7e59af6594e1
say811007 <say831007@gmail.com>
2025-09-03 19:20:29 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,10 @@
-const express = require('express');
-const line = require('@line/bot-sdk');
-
-const config = {
-  channelAccessToken: process.env.LINE_CHANNEL_TOKEN,
-  channelSecret: process.env.LINE_CHANNEL_SECRET
-};
-
-const client = new line.Client(config);
-const app = express();
-app.use(express.json());
-
 app.post('/webhook', line.middleware(config), (req, res) => {
   Promise
     .all(req.body.events.map(handleEvent))
-    .then(result => res.json(result));
+    .then((result) => res.status(200).json(result))
+    .catch((err) => {
+      console.error(err);
+      res.status(500).end();
+    });
 });
 
-function handleEvent(event) {
-  if (event.type !== 'message' || event.message.type !== 'text') {
-    return Promise.resolve(null);
-  }
-  return client.replyMessage(event.replyToken, {
-    type: 'text',
-    text: `你說了：${event.message.text}`
-  });
-}
-
-app.listen(3000, () => console.log('Server running'));
